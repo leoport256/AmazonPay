8715e9f54d9189bfc9a2787e6c9fc24f8bb015b2
Leonid Kokurin <mentarchik@gmail.com>
2024-10-19 22:06:30 +0300

    Remove StringBuilder .

--- AmazonPayHttpClient/Signing/StringToSignBuilder.cs
+++ AmazonPayHttpClient/Signing/StringToSignBuilder.cs
@@ -6,19 +6,11 @@ internal static  class StringToSignBuilder
 {
 	private const string LineSeparator = "\n";
 	private const string AmazonSignatureAlgorithm = "AMZN-PAY-RSASSA-PSS-V2";
+	private const string AmazonSignatureAlgorithmWithNewLine = $"{AmazonSignatureAlgorithm}{LineSeparator}";
 
 	public static string Build(IHasher hasher, string request)
 	{
 		var hashed = hasher.FormattedHash(request);
-
-		var stringToSignBuilder = new StringBuilder();
-		
-		// TODO: I think this is fixed length string
-		stringToSignBuilder.
-			Append(AmazonSignatureAlgorithm).
-			Append(LineSeparator).
-			Append(hashed);
-
-		return stringToSignBuilder.ToString();
+		return string.Concat(AmazonSignatureAlgorithmWithNewLine.AsSpan(), hashed.AsSpan());
 	}
 }
