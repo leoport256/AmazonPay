176f17d2711c50e2407ff5574fc7ee788fc66f8c
Leo Port <leoport256@gmail.com>
2025-06-06 00:02:55 +0300

    Add newtonsoft refit sample for get with query method.

---
+++ .samples/dotnet8/Newtonsoft/Refit/AmazonPay.Samples.Dotnet8.Newtonsoft.Refit.GetReports/IUnitedStatesAmazonPayClient.cs
@@ -0,0 +1,19 @@
+﻿using AmazonPayHttpClient.Contracts.Newtonsoft;
+using AmazonPayHttpClient.Refit.Newtonsoft;
+using Refit;
+
+namespace AmazonPay.Samples.Dotnet8.Newtonsoft.Refit.GetReports;
+
+public interface IUnitedStatesAmazonPayClient: IAmazonPayClient
+{
+	[Get("/v2/reports")]
+	Task<ApiResponse<GetReportsResponse>> GetReportsWithFailSignature(
+		[Query(CollectionFormat.Csv)][AliasAs("processingStatuses")]ProcessingStatus[]? processingStatuses = null,
+
+		[Query(Format = AmazonDateTimeFormat.Format)][AliasAs("createdSince")]DateTime? createdSince = null, 
+		[Query(Format = AmazonDateTimeFormat.Format)][AliasAs("createdUntil")]DateTime? createdUntil = null,
+		[Query][AliasAs("nextToken")]string? nextToken = null,
+		[Query][AliasAs("pageSize")]int pageSize = 10, 
+		[Query(CollectionFormat.Csv)][AliasAs("reportTypes")]ReportTypes[]? reportTypes = null);
+
+}
